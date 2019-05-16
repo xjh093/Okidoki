d9052628fe5c500c93ca880f3f1f7248a66269a5
HaoCold <xjh093@126.com>
2019-05-16 10:22:24 +0800

    Update Okidoki.m

--- Okidoki/Okidoki.m
+++ Okidoki/Okidoki.m
@@ -50,7 +50,8 @@
 #pragma mark - UIView
 
 kOkidoki_imp(tag, ({
-    if ([tag isKindOfClass:[NSNumber class]]) {
+    if ([tag isKindOfClass:[NSNumber class]] ||
+        [tag isKindOfClass:[NSString class]]) {
         view.tag = [tag integerValue];
     }
 }))
@@ -58,11 +59,14 @@ kOkidoki_imp(tag, ({
 kOkidoki_imp(frame, ({
     if ([frame isKindOfClass:[NSValue class]]) {
         view.frame = [frame CGRectValue];
+    }else if ([frame isKindOfClass:[NSString class]]) {
+        
     }
 }))
 
 kOkidoki_imp(alpha, ({
-    if ([alpha isKindOfClass:[NSNumber class]]) {
+    if ([alpha isKindOfClass:[NSNumber class]] ||
+        [alpha isKindOfClass:[NSString class]]) {
         view.alpha = [alpha floatValue];
     }
 }))
@@ -76,19 +80,22 @@ kOkidoki_imp(bdColor, ({
 }))
 
 kOkidoki_imp(bdWidth, ({
-    if([bdWidth isKindOfClass:[NSNumber class]]){
+    if([bdWidth isKindOfClass:[NSNumber class]] ||
+       [bdWidth isKindOfClass:[NSString class]]){
         view.layer.borderWidth = [bdWidth floatValue];
     }
 }))
 
 kOkidoki_imp(cnRadius, ({
-    if([cnRadius isKindOfClass:[NSNumber class]]){
+    if([cnRadius isKindOfClass:[NSNumber class]] ||
+       [cnRadius isKindOfClass:[NSString class]]){
         view.layer.cornerRadius = [cnRadius floatValue];
     }
 }))
 
 kOkidoki_imp(mtBounds, ({
-    if([mtBounds isKindOfClass:[NSNumber class]]){
+    if([mtBounds isKindOfClass:[NSNumber class]] ||
+       [mtBounds isKindOfClass:[NSString class]]){
         view.layer.masksToBounds = [mtBounds boolValue];
     }
 }))
@@ -102,51 +109,70 @@ kOkidoki_imp(text, ({
 }))
 
 kOkidoki_imp(font, ({
-    if ([font isKindOfClass:[UIFont class]]) {
+    if ([font isKindOfClass:[UIFont class]] ||
+        [font isKindOfClass:[NSString class]]) {
         if ([view isKindOfClass:[UIButton class]]) {
             UIButton *button = (UIButton *)view;
-            button.titleLabel.font = font;
+            button.titleLabel.font = [UIFont okidokiFont:font];
         }else if ([view respondsToSelector:@selector(setFont:)]) {
-            [view performSelector:@selector(setFont:) withObject:font];
+            [view performSelector:@selector(setFont:) withObject:[UIFont okidokiFont:font]];
         }
     }
 }))
 
 kOkidoki_imp(color, ({
-    if([view respondsToSelector:@selector(setTextColor:)]){
+    if ([view isKindOfClass:[UIButton class]]) {
+        UIButton *button = (UIButton *)view;
+        [button setTitleColor:[UIColor okidokiColor:color] forState:0];
+    }else if([view respondsToSelector:@selector(setTextColor:)]){
         [view performSelector:@selector(setTextColor:) withObject:[UIColor okidokiColor:color]];
     }
 }))
 
 kOkidoki_imp(align, ({
-    if([align isKindOfClass:[NSNumber class]]){
-        if([view respondsToSelector:@selector(setTextAlignment:)]){
-            [view performSelector:@selector(setTextAlignment:) withObject:align];
+    if([align isKindOfClass:[NSNumber class]] ||
+       [align isKindOfClass:[NSString class]]){
+        if ([view isKindOfClass:[UIButton class]]) {
+            UIButton *button = (UIButton *)view;
+            button.titleLabel.textAlignment = [align integerValue];
+        }else if([view respondsToSelector:@selector(setTextAlignment:)]){
+            [view setValue:align forKey:@"textAlignment"];
         }
     }
 }))
 
 kOkidoki_imp(lines, ({
-    if([view isKindOfClass:[UILabel class]] &&
-       [lines isKindOfClass:[NSNumber class]]){
-        [(UILabel *)view setNumberOfLines:[lines integerValue]];
+    if(([lines isKindOfClass:[NSNumber class]] ||
+        [lines isKindOfClass:[NSString class]])){
+        if ([view isKindOfClass:[UILabel class]]) {
+            [(UILabel *)view setNumberOfLines:[lines integerValue]];
+        }else if ([view isKindOfClass:[UIButton class]]) {
+            UIButton *button = (UIButton *)view;
+            button.titleLabel.numberOfLines = [lines integerValue];
+        }
     }
 }))
 
 kOkidoki_imp(adjust, ({
-    if([view isKindOfClass:[UILabel class]] &&
-       [adjust isKindOfClass:[NSNumber class]]){
-        [(UILabel *)view setNumberOfLines:[adjust boolValue]];
+    if(([adjust isKindOfClass:[NSNumber class]] ||
+        [adjust isKindOfClass:[NSString class]])){
+       if ([view isKindOfClass:[UILabel class]]) {
+           [(UILabel *)view setAdjustsFontSizeToFitWidth:[adjust boolValue]];
+       }else if ([view isKindOfClass:[UIButton class]]) {
+           UIButton *button = (UIButton *)view;
+           button.titleLabel.adjustsFontSizeToFitWidth = [adjust integerValue];
+       }
     }
 }))
 
 kOkidoki_imp(lineSpace, ({
     if([view isKindOfClass:[UILabel class]] &&
-       [lineSpace isKindOfClass:[NSNumber class]]){
+       ([lineSpace isKindOfClass:[NSNumber class]] ||
+        [lineSpace isKindOfClass:[NSString class]])){
         UILabel *label = (UILabel *)view;
         CGFloat space = [lineSpace floatValue];
         
-        NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithAttributedString:label.attributedText];
+        NSMutableAttributedString *attStr = [[NSMutableAttributedString alloc] initWithAttributedString:label.attributedText?:[[NSAttributedString alloc] initWithString:label.text]];
         NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
         [paragraphStyle setLineSpacing:space < 0 ? 0 : (space - (label.font.lineHeight - label.font.pointSize))];
         paragraphStyle.alignment = label.textAlignment;
@@ -202,6 +228,9 @@ kOkidoki_imp(autoHeight, ({
             
             NSString *text = [view performSelector:@selector(text)];
             NSAttributedString *attributedText = [view performSelector:@selector(attributedText)];
+            if (!attributedText) {
+                attributedText = [[NSAttributedString alloc] initWithString:text];
+            }
             
             // color
             if ([value isKindOfClass:[UIColor class]]) {
@@ -228,7 +257,11 @@ kOkidoki_imp(autoHeight, ({
         
         if ([view respondsToSelector:@selector(attributedText)]) {
             
+            NSString *text = [view performSelector:@selector(text)];
             NSAttributedString *attributedText = [view performSelector:@selector(attributedText)];
+            if (!attributedText) {
+                attributedText = [[NSAttributedString alloc] initWithString:text];
+            }
             
             // color
             if ([value isKindOfClass:[UIColor class]] &&
@@ -251,6 +284,13 @@ kOkidoki_imp(autoHeight, ({
 }
 
 #pragma mark - UIButton
+
+kOkidoki_imp(title, ({
+    if ([view isKindOfClass:[UIButton class]] &&
+        [title isKindOfClass:[NSString class]]) {
+        [(UIButton *)view setTitle:title forState:0];
+    }
+}))
 
 - (Okidoki*(^)(id,id))titleForState{
     return ^id(id title, id state){
@@ -482,6 +522,13 @@ kOkidoki_imp(image, ({
         }else if ([image isKindOfClass:[UIImage class]]){
             [view performSelector:@selector(setImage:) withObject:image];
         }
+    }else if ([view isKindOfClass:[UIButton class]]) {
+        UIButton *button = (UIButton *)view;
+        if ([image isKindOfClass:[NSString class]]) {
+            [button setImage:[UIImage imageNamed:image] forState:0];
+        }else if ([image isKindOfClass:[UIImage class]]){
+            [button setImage:image forState:0];
+        }
     }
 }))
 
@@ -510,8 +557,9 @@ kOkidoki_imp(image, ({
 
 kOkidoki_imp(bdStyle, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [bdStyle isKindOfClass:[NSNumber class]]) {
-        [view performSelector:@selector(setBorderStyle:) withObject:bdStyle];
+        ([bdStyle isKindOfClass:[NSNumber class]]) ||
+         [bdStyle isKindOfClass:[NSString class]]) {
+        [(UITextField *)view setBorderStyle:[bdStyle integerValue]];
     }
 }))
 
@@ -530,29 +578,33 @@ kOkidoki_imp(pHColor, ({
 
 kOkidoki_imp(pHFont, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [pHFont isKindOfClass:[UIFont class]]) {
-        [self setValue:pHFont forKeyPath:@"_placeholderLabel.font"];
+        ([pHFont isKindOfClass:[UIFont class]] ||
+         [pHFont isKindOfClass:[NSString class]])) {
+        [self setValue:[UIFont okidokiFont:pHFont] forKeyPath:@"_placeholderLabel.font"];
     }
 }))
 
 kOkidoki_imp(cbMode, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [cbMode isKindOfClass:[NSNumber class]]) {
-        [view performSelector:@selector(setClearButtonMode:) withObject:cbMode];
+        ([cbMode isKindOfClass:[NSNumber class]] ||
+         [cbMode isKindOfClass:[NSString class]])) {
+        [(UITextField *)view setClearButtonMode:[cbMode integerValue]];
     }
 }))
 
 kOkidoki_imp(lvMode, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [lvMode isKindOfClass:[NSNumber class]]) {
-        [view performSelector:@selector(setLeftViewMode:) withObject:lvMode];
+        ([lvMode isKindOfClass:[NSNumber class]] ||
+         [lvMode isKindOfClass:[NSString class]])) {
+        [(UITextField *)view setLeftViewMode:[lvMode integerValue]];
     }
 }))
 
 kOkidoki_imp(rvMode, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [rvMode isKindOfClass:[NSNumber class]]) {
-        [view performSelector:@selector(setRightViewMode:) withObject:rvMode];
+        ([rvMode isKindOfClass:[NSNumber class]] ||
+         [rvMode isKindOfClass:[NSString class]])) {
+        [(UITextField *)view setRightViewMode:[rvMode integerValue]];
     }
 }))
 
@@ -572,8 +624,49 @@ kOkidoki_imp(rtView, ({
 
 kOkidoki_imp(secure, ({
     if ([view isKindOfClass:[UITextField class]] &&
-        [secure isKindOfClass:[NSNumber class]]) {
-        [view performSelector:@selector(setSecureTextEntry:) withObject:secure];
+        ([secure isKindOfClass:[NSNumber class]] ||
+         [secure isKindOfClass:[NSString class]])) {
+        [(UITextField *)view setSecureTextEntry:[secure boolValue]];
+    }
+}))
+
+#pragma mark - UIScrollView
+
+kOkidoki_imp(verInd, ({
+    if ([view isKindOfClass:[UIScrollView class]]) {
+        [(UIScrollView *)view setShowsVerticalScrollIndicator:[verInd boolValue]];
+    }
+}))
+
+kOkidoki_imp(horInd, ({
+    if ([view isKindOfClass:[UIScrollView class]]) {
+        [(UIScrollView *)view setShowsHorizontalScrollIndicator:[horInd boolValue]];
+    }
+}))
+
+kOkidoki_imp(paging, ({
+    if ([view isKindOfClass:[UIScrollView class]]) {
+        [(UIScrollView *)view setPagingEnabled:[paging boolValue]];
+    }
+}))
+
+kOkidoki_imp(bounces, ({
+    if ([view isKindOfClass:[UIScrollView class]]) {
+        [(UIScrollView *)view setBounces:[bounces boolValue]];
+    }
+}))
+
+#pragma mark - UITextView
+
+kOkidoki_imp(editable, ({
+    if ([view isKindOfClass:[UITextView class]]) {
+        [(UITextView *)view setEditable:[editable boolValue]];
+    }
+}))
+
+kOkidoki_imp(selectable, ({
+    if ([view isKindOfClass:[UITextView class]]) {
+        [(UITextView *)view setSelectable:[selectable boolValue]];
     }
 }))
 
@@ -652,6 +745,34 @@ kOkidoki_imp(secure, ({
     UIColor *xxColor = [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1];
 
     return xxColor;
+}
+
+@end
+
+@implementation UIFont (Okidoki)
+
++ (UIFont *)okidokiFont:(id)font
+{
+    if ([font isKindOfClass:[UIFont class]]) {
+        return (UIFont *)font;
+    }else if ([font isKindOfClass:[NSString class]]) {
+        NSString *string = (NSString *)font;
+        UIFont *theFont = [UIFont systemFontOfSize:[UIFont systemFontSize]];
+        if ([string floatValue] > 0) {
+            theFont = [UIFont systemFontOfSize:[string floatValue]];
+        }else if (string.length > 1) {
+            if ([string hasPrefix:@"s"]){
+                theFont =  [UIFont systemFontOfSize:[[string substringFromIndex:1] floatValue]];
+            }else if ([string hasPrefix:@"b"]){
+                theFont =  [UIFont boldSystemFontOfSize:[[string substringFromIndex:1] floatValue]];
+            }else if ([string hasPrefix:@"i"]){
+                theFont =  [UIFont italicSystemFontOfSize:[[string substringFromIndex:1] floatValue]];
+            }
+        }
+        return theFont;
+    }
+    
+    return [UIFont systemFontOfSize:[UIFont systemFontSize]];;
 }
 
 @end
